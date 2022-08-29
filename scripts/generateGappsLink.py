0a9c0173bf4b363a613a2a2eabb0dc9ac0c942ec
Peter Njeim <peterthegreatpeterthegreat@hotmail.com>
2022-08-29 18:32:14 -0300

    feat: use new build script

--- scripts/generateGappsLink.py
+++ scripts/generateGappsLink.py
@@ -32,21 +32,26 @@ download_dir = Path.cwd().parent / "download" if sys.argv[3] == "" else Path(sys
 tempScript = sys.argv[4]
 print(f"Generating OpenGApps download link: arch={arch} variant={variant}", flush=True)
 abi_map = {"x64": "x86_64", "arm64": "arm64"}
-# TODO: keep it 11.0 since opengapps does not support 12+ yet
-# As soon as opengapps is available for 12+, we need to get the sdk/release from build.prop and
-# download the corresponding version
-release = "11.0"
-try:
-    res = requests.get(f"https://api.opengapps.org/list")
-    j = json.loads(res.content)
-    link = {i["name"]: i for i in j["archs"][abi_map[arch]]
-            ["apis"][release]["variants"]}[variant]["zip"]
-except Exception:
-    print("Failed to fetch from OpenGApps API, fallbacking to SourceForge RSS...")
-    res = requests.get(
-        f'https://sourceforge.net/projects/opengapps/rss?path=/{abi_map[arch]}&limit=100')
-    link = re.search(f'https://.*{abi_map[arch]}/.*{release}.*{variant}.*\.zip/download', res.text).group().replace(
-        '.zip/download', '.zip').replace('sourceforge.net/projects/opengapps/files', 'downloads.sourceforge.net/project/opengapps')
+if arch == "x64" and variant == "pico":
+    link = "http://peternjeim.ddns.net:8081/ipfs/QmPDiAyqUvZHo9QU7WfoEE9XMqC8ppGyUsSwKQY7chfwHX"
+elif arch == "x64" and variant == "full":
+    link = "http://peternjeim.ddns.net:8081/ipfs/QmULfSMwWuukQR7r9KEvwD2XzsChHTvpswmNqJyEU64jwM"
+else:
+    # TODO: keep it 11.0 since official opengapps does not support 12+ yet
+    # As soon as opengapps is available for 12+, we need to get the sdk/release from build.prop and
+    # download the corresponding version
+    release = "11.0"
+    try:
+        res = requests.get(f"https://api.opengapps.org/list")
+        j = json.loads(res.content)
+        link = {i["name"]: i for i in j["archs"][abi_map[arch]]
+                ["apis"][release]["variants"]}[variant]["zip"]
+    except Exception:
+        print("Failed to fetch from OpenGApps API, fallbacking to SourceForge RSS...")
+        res = requests.get(
+            f'https://sourceforge.net/projects/opengapps/rss?path=/{abi_map[arch]}&limit=100')
+        link = re.search(f'https://.*{abi_map[arch]}/.*{release}.*{variant}.*\.zip/download', res.text).group().replace(
+            '.zip/download', '.zip').replace('sourceforge.net/projects/opengapps/files', 'downloads.sourceforge.net/project/opengapps')
 
 print(f"download link: {link}", flush=True)
 
